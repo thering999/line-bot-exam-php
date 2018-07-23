6d285887f70f78f1a5abc9b070d0eb78dc6acf63
thering999 <the_ring99@hotmail.com>
2018-07-23 11:45:47 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '2vs4xFjqSoDgvxLIYbZHPr/Q6eVt+XQWVvouCemdHHyG3p9gv3y7VYqNwozCSNHCFAjAbR441eNQkGDSTsyqIyq8I8+uwv629L+E/DqE5m9ubTtPyeihPymLhqnke/uycoMcAbSL+OWw7GyoHG1EdgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
